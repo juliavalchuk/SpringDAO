286a4e1ae9e1087fb46df4b4c6ccf645aa10bffe
Julia Valchuk <julia.valchuk@gmail.com>
2014-04-24 14:01:23 +0300

    add bookRepositoryTest

--- src/test/java/ua/epam/spring/library/book/BookRepositoryJDBCTest.java
+++ src/test/java/ua/epam/spring/library/book/BookRepositoryJDBCTest.java
@@ -2,14 +2,19 @@ package ua.epam.spring.library.book;
 
 import org.junit.After;
 import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.test.context.ContextConfiguration;
 import ua.epam.spring.library.RepositoryTest;
 
 /**
  * Created by Yuliia_Valchuk on 4/23/2014.
  */
+@ContextConfiguration(locations = {"/connectionContextTest.xml","/bookContex.xml"})
+public class BookRepositoryJDBCTest extends RepositoryTest {
+    @Autowired
+    BookRepository bookRepository;
 
-public class BookRepositoryJDBCTest {
     @Before
     public void setUp() throws Exception {
 
